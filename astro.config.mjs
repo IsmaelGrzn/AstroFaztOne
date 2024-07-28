7fbed10963ddbb7dc0fdc4042cd3837853b47597
isma20025 <isma20025@gmail.com>
2024-07-28 16:33:05 +0000

    astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://IsmaelGrzn.github.io',
+  base: 'AstroFaztOne',
   integrations: [tailwind()]
 });
